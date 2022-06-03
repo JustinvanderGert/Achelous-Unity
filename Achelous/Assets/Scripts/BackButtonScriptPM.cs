cc47c95b088b5950b1eab84f6eaf6ca25dc99109
Calamari <95355083+calamariichi@users.noreply.github.com>
2022-06-03 13:15:04 +0200

    Inserted Underwater Frog prefab draft

--- Achelous/Assets/Scripts/BackButtonScriptPM.cs
+++ Achelous/Assets/Scripts/BackButtonScriptPM.cs
@@ -1,44 +1,45 @@
-    using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//    using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class BackButtonScriptPM : MonoBehaviour
-{
-    public returnSystem;
+//public class BackButtonScriptPM : MonoBehaviour
+//{
+//    //public returnSystem
+//    //nicolo was here
 
-    public GameObject PauseMenuHUD;
-    public GameObject InGameHUD;
-    public Camera AimCamera;
+//    public GameObject PauseMenuHUD;
+//    public GameObject InGameHUD;
+//    public Camera AimCamera;
 
-    void Update()
-    {
-        if (returnSystem)
-        {
-            gameIsPaused = !gameIsPaused;
-            PauseGame();
+//    void Update()
+//    {
+//        if (returnSystem)
+//        {
+//            gameIsPaused = !gameIsPaused;
+//            PauseGame();
 
-        }
-    }
-    void PauseGame()
-    {
-        if (gameIsPaused)
-        {
-            Time.timeScale = 0f;
-            PauseMenuHUD.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            AimCamera.gameObject.SetActive(false);
-            InGameHUD.SetActive(false);
-        }
-        else
-        {
-            Time.timeScale = 1;
-            PauseMenuHUD.SetActive(false);
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            AimCamera.gameObject.SetActive(true);
-            InGameHUD.SetActive(true);
-        }
-    }
-}
+//        }
+//    }
+//    void PauseGame()
+//    {
+//        if (gameIsPaused)
+//        {
+//            Time.timeScale = 0f;
+//            PauseMenuHUD.SetActive(true);
+//            Cursor.lockState = CursorLockMode.None;
+//            Cursor.visible = true;
+//            AimCamera.gameObject.SetActive(false);
+//            InGameHUD.SetActive(false);
+//        }
+//        else
+//        {
+//            Time.timeScale = 1;
+//            PauseMenuHUD.SetActive(false);
+//            Cursor.lockState = CursorLockMode.Locked;
+//            Cursor.visible = false;
+//            AimCamera.gameObject.SetActive(true);
+//            InGameHUD.SetActive(true);
+//        }
+//    }
+//}
    
